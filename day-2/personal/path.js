221a371f5e01ed83975c338b0d003e558772e927
mrullldhm <mrullldhm94>
2024-12-18 03:16:42 +0800

    day-2 personal path.js

---
+++ day-2/personal/path.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+// path.join([...paths])
+const textPath = path.join(__dirname,`fs.file`, `message.txt`);
+console.log(textPath);
+
+// path.extname(path)
+const extFile = path.extname(textPath);
+console.log(extFile);
+
+// Check txt file
+if (extFile === ".txt") {
+  console.log("This is a text file");
+} else {
+  console.log("This is not a text file");
+}
+
+// Check jpeg file
+if (extFile === ".jpeg") {
+    console.log("This is a jpeg file");
+  } else {
+    console.log("This is not a jpeg file");
+  }
+
+console.log(__dirname);
+console.log(__filename);
